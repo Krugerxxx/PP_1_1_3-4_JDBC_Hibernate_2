5240e161104a7617e0f6595801d6d22502074090
Krugerxxx <sssnik@mail.ru>
2022-08-17 14:17:06 +0500

    Result

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,117 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.QueryTimeoutException;
+import javax.persistence.RollbackException;
+import javax.persistence.TransactionRequiredException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final String TABLE_USERS = "users";
+    private Util util = Util.getInstance();
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = util.getSession()) {
+            String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_USERS +
+                    "(id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR (30) NOT NULL, " +
+                    "lastName VARCHAR (30) NOT NULL, " +
+                    "age SMALLINT NOT NULL)";
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (IllegalStateException | PersistenceException e) {
+            System.out.println("Ошибка при создании таблицы");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = util.getSession()) {
+            String sql = "DROP TABLE " + TABLE_USERS;
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (IllegalStateException | PersistenceException e) {
+            System.out.println("Ошибка при удалении таблицы");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = util.getSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+        } catch (IllegalStateException | PersistenceException e) {
+            System.out.println("Ошибка при добалении данных в таблицу");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = util.getSession()) {
+            session.beginTransaction();
+            User user = session.load(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        } catch (IllegalStateException | PersistenceException e) {
+            System.out.println("Ошибка при удалении данных из таблицы");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        try (Session session = util.getSession()) {
+            return session.createQuery("FROM User").getResultList();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка при чтении данных");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = util.getSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (IllegalStateException | PersistenceException e) {
+            System.out.println("Ошибка при очистке таблицы");
+            StackTraceElement[] element = e.getStackTrace();
+            for (StackTraceElement o : element) {
+                System.out.println(o);
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
+    private static Util util;
     private final String NAME_USER = "root";
     private final String PASSWORD = "root";
     private final String URL = "jdbc:mysql://localhost:3306/mysql";
-    private static Util util;
     private Connection connection;
 
-    private Util(){}
+    private SessionFactory sessionFactory;
+
+    private Util() {
+    }
+
+    public static synchronized Util getInstance() {
+        if (util == null) {
+            util = new Util();
+        }
+        return util;
+    }
 
     public Connection getConnection() throws SQLException {
         connection = DriverManager.getConnection(URL, NAME_USER, PASSWORD);
@@ -17,12 +38,26 @@ public class Util {
         return connection;
     }
 
-
-
-    public static synchronized Util getInstance() {
-        if(util == null){
-            util = new Util();
+    public Session getSession() throws HibernateException{
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.setProperty(Environment.URL, URL);
+            properties.setProperty(Environment.USER, NAME_USER);
+            properties.setProperty(Environment.PASS, PASSWORD);
+            properties.setProperty(Environment.POOL_SIZE, "1");
+            properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            properties.setProperty(Environment.SHOW_SQL, "true");
+            configuration.setProperties(properties);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            sessionFactory = new MetadataSources(serviceRegistry)
+                    .addAnnotatedClass(User.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
         }
-        return util;
+        return sessionFactory.openSession();
     }
+
 }
